b339133a946724cc6dcccb14e7f7c5276532f239
Thinking Song <123wuming_123@163.com>
2016-11-28 12:11:46 +0800

    set language to zh-CN

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'rails-i18n', '~> 5.0.0'
+
 # Use mysql2 as the database for Active Record
 gem 'mysql2'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Transport
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.time_zone = 'Beijing'
+    config.i18n.default_locale = 'zh-CN'
     config.active_job.queue_adapter = :sidekiq
   end
 end
