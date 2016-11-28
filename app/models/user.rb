a0583696ecb791d3e38897d993a442b56ead58d0
Thinking Song <123wuming_123@163.com>
2016-11-28 18:35:35 +0800

    create user

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+        :confirmable, :lockable, :timeoutable and :omniauthable
+end
